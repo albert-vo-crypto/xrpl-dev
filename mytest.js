02cebb1b136c45e4e8c60c3038dc1bf2c9ecb225
Albert Vo <albert.vo.crypto@gmail.com>
2022-02-25 15:34:15 +0000

    update seller

---
+++ mytest.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+const xrpl = require("xrpl")
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/mytest5.html'));
+  //__dirname : It will resolve to your project folder.
+});
+app.listen(3011);
