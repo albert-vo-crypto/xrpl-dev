2a85309bb5e67732005b2deb89909504b584cbdf
Albert Vo <albert.vo.crypto@gmail.com>
2022-02-25 22:14:14 +0000

    add myseller12.html with enhanced logging

--- seller.js
+++ seller.js
@@ -2,12 +2,11 @@ var express = require("express");
 var app = express();
 var path = require("path");
 const xrpl = require("xrpl")
-const apiKey = process.env.API_KEY
-console.log(apiKey)
+
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/seller.html'));
   //__dirname : It will resolve to your project folder.
-};
+});
 app.listen(3011);
 
